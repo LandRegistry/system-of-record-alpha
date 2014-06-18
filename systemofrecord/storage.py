cf1e66254f2e904c721830b9cead88dc7aad257b
Adam Shimali <adam.shimali@gmail.com>
2014-06-18 10:41:39 +0100

    fixed property name for hash which should have been sha256

--- systemofrecord/storage.py
+++ systemofrecord/storage.py
@@ -17,10 +17,10 @@ class Storage(object,):
        #TODO check data integrity using public key
        bucket = self.__get_bucket()
 
-       key_path = "%s/%s.json" % (data['title_identifier'], data['sha256sum'])
+       key_path = "%s/%s.json" % (data['title_number'], data['sha256'])
        key = bucket.new_key(key_path)
        key.set_contents_from_string(json.dumps(data), encrypt_key=True)
-       self.__set_new_head(data['title_identifier'], bucket, key_path)
+       self.__set_new_head(data['title_number'], bucket, key_path)
 
     def __set_new_head(self, title_number, source_bucket, source_key):
         destination_key  = "%s/%s.json" % (title_number,  'head')
