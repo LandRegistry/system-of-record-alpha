c418e3b78379adb9dd452a6220cd275cabf57b2e
Juan Matthys Uys <opyate@gmail.com>
2014-06-19 11:32:42 +0100

    use ts as file name

--- systemofrecord/storage.py
+++ systemofrecord/storage.py
@@ -17,7 +17,7 @@ class Storage(object,):
        #TODO check data integrity using public key
        bucket = self.__get_bucket()
 
-       key_path = "%s/%s.json" % (data['title_number'], data['sha256'])
+       key_path = "%s/%s.json" % (data['title_number'], data['title']['created_ts'])
        key = bucket.new_key(key_path)
        key.set_contents_from_string(json.dumps(data), encrypt_key=True)
        self.__set_new_head(data['title_number'], bucket, key_path)
