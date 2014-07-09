1e6442c315b9bd8002eb2d94d5c36fd5aa167fc5
Juan Matthys Uys <opyate@gmail.com>
2014-07-09 11:26:19 +0100

    fix tests

--- tests/test_system_of_record.py
+++ tests/test_system_of_record.py
@@ -1,7 +1,7 @@
 import calendar
 import datetime
 from systemofrecord import server
-from systemofrecord.storage import DBStore
+from systemofrecord.storage import DBStore, MemoryStore
 import unittest
 import json
 
@@ -9,75 +9,69 @@ test_creation_time = calendar.timegm(datetime.datetime.utcnow().timetuple())
 the_past = datetime.datetime.now() - datetime.timedelta(days=30)
 some_past_timestamp = calendar.timegm(the_past.timetuple())
 
+
 class SystemOfRecordTestCase(unittest.TestCase):
 
-    def test_nothing(self):
-        self.assertEquals(True, True)
+    def setUp(self):
 
-    # def setUp(self):
+        server.app.config['TESTING'] = True
+        self.app = server.app.test_client()
+        #make sure we're running against in memory store
+        if type(server.storage) == DBStore:
+            server.storage = MemoryStore()
+            print "DB was used, switched to memory store"
 
-    #     server.app.config['TESTING'] = True
-    #     self.app = server.app.test_client()
-    #     #make sure we're running against in memory store
-    #     if type(server.storage) == DBStore:
-    #         server.storage = MemoryStore()
-    #         print "DB was used, switched to memory store"
+    def tearDown(self):
+        server.storage.clear()
 
-    # def tearDown(self):
-    #     server.storage.clear()
+    def test_empty_store(self):
+        response = self.app.get('/titles')
+        self.assertEqual(json.loads(response.data),  {'titles': []})
 
-    # def test_empty_store(self):
-    #     response = self.app.get('/titles')
-    #     self.assertEqual(json.loads(response.data),  {'titles': []})
+    def test_add_title(self):
+        title_number = "TN1234567"
+        self.app.post('/titles/last', data=json.dumps(
+            {
+                "title_number": title_number,
+                "address": "The Hovel, Moletown",
+                "created_ts": test_creation_time,
+                "predecessor": None
+            }), content_type='application/json')
 
-    # def test_add_title(self):
-    #     title_number = "TN1234567"
-    #     self.app.post('/titles/last', data=json.dumps(
-    #         {
-    #             "title_number": title_number,
-    #             "address": "The Hovel, Moletown",
-    #             "created_ts": test_creation_time,
-    #             "predecessor": None
-    #         }), content_type='application/json')
+        response = self.app.get('/titles/last')
 
-    #     response = self.app.get('/titles/last')
+        self.assertEqual(json.loads(response.data), {
+            "title_number": title_number,
+            "address": "The Hovel, Moletown",
+            "created_ts": test_creation_time,
+            "predecessor": None
+        })
 
-    #     self.assertEqual(json.loads(response.data), {
-    #                 "title_number": title_number,
-    #                 "address": "The Hovel, Moletown",
-    #                 "created_ts": test_creation_time,
-    #                 "predecessor": None
+    def test_get_last_title(self):
+        title_number = "TN1234567"
 
-    #         })
+        #create two records
+        self.app.post('/titles/last', data=json.dumps({
+            "title_number": "ANUMB3R",
+            "address": "The Hovel, Moletown",
+            "created_ts": some_past_timestamp
+        }), content_type='application/json')
 
-    # def test_get_last_title(self):
-    #     title_number = "TN1234567"
+        #add another - this will be last now
+        self.app.post('/titles/last', data=json.dumps({
+            "title_number": title_number,
+            "address": "The Palace, Kings Landing",
+            "created_ts": test_creation_time
+        }), content_type='application/json')
 
-    #     #create two records
-    #     self.app.post('/titles/last', data=json.dumps(
-    #         {
-    #             "title_number": "ANUMB3R",
-    #             "address": "The Hovel, Moletown",
-    #             "created_ts": some_past_timestamp
-    #         }), content_type='application/json')
+        # get the last record
+        response = self.app.get('/titles/last')
 
-    #     #add another - this will be last now
-    #     self.app.post('/titles/last', data=json.dumps(
-    #         {
-    #             "title_number": title_number,
-    #             "address": "The Palace, Kings Landing",
-    #             "created_ts": test_creation_time
-    #         }), content_type='application/json')
-
-    #     # get the last record
-    #     response = self.app.get('/titles/last')
-
-    #     self.assertEqual(json.loads(response.data), {
-    #            "title_number": title_number,
-    #             "address": "The Palace, Kings Landing",
-    #             "created_ts": test_creation_time
-    #     })
-
+        self.assertEqual(json.loads(response.data), {
+            "title_number": title_number,
+            "address": "The Palace, Kings Landing",
+            "created_ts": test_creation_time
+        })
 
 
 if __name__ == '__main__':
