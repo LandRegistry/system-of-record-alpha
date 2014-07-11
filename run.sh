a40cc4b1a7a222b26e2a63811c5fb87f6f82ccf0
Matthew Wall <mwall@alphagov.co.uk>
2014-07-11 08:17:23 +0100

    Exported HTTP_URL before we start the app rather than after it

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 export SYSTEMOFRECORD_URL=foo
 export SETTINGS='config.Config'
 export DATABASE_URL='postgresql://localhost/sysofrec'
+export HTTP_URL=0.0.0.0:5000
 
 foreman start
-export HTTP_URL=0.0.0.0:5000
 
