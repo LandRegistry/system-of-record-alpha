02d3acd63e21aaa12348bd90ab6eb1e81cf6f922
Adam Shimali <adam.shimali@gmail.com>
2014-07-15 15:20:38 +0100

    fixed borked test

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config(object):
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    REDIS_HOST = os.environ.get('REDIS_HOST')
+    REDIS_HOST = os.environ.get('REDIS_HOST', 'redis://user:@localhost:6379') #please let this pass for now :)
     REDIS_QUEUE_KEY = os.environ.get('REDIS_QUEUE_KEY')
 
 class DevelopmentConfig(Config):
