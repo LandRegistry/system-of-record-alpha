8b9d235c5611549f26f8540db236e31b6cd54824
Skablam <stainlesssteelmatt@gmail.com>
2014-07-15 23:14:02 +0100

    remove unicode characters

--- systemofrecord/feeder.py
+++ systemofrecord/feeder.py
@@ -11,9 +11,21 @@ class FeederQueue(object):
 
     def enqueue(self, number, json):
         try:
-            self.redis.rpush(self.redis_queue, json)
+            json_ascii = convert_to_ascii(json)
+            self.redis.rpush(self.redis_queue, json_ascii)
             self.logger .info("Enqueued data for title %s: data: %s" % (number, json))
             self.logger.info("Redis: %s" % self.redis)
         except Exception, e:
             self.logger .info(e)
             self.logger .info("Couldn't enqueue data for title %s: data: %s" % (number, json))
+
+
+def convert_to_ascii(input):
+    if isinstance(input, dict):
+        return {convert_to_ascii(key): convert_to_ascii(value) for key, value in input.iteritems()}
+    elif isinstance(input, list):
+        return [convert_to_ascii(element) for element in input]
+    elif isinstance(input, unicode):
+        return input.encode('utf-8')
+    else:
+        return input
