37220413cd3d16db4b6ae45d3e127621c63f2b5a
Adam Shimali <adam.shimali@gmail.com>
2014-07-15 22:25:52 +0100

    Added mocking for test to isolate redis and db
    Also added example to illustrate using in memory sql db
    initialised in test setup. So memory store in storage module not needed.
    
    Commented out get_last from server and test. Needs revisting as last is no longer
    at head of a single list of titles.
    
    Moved mock to requirements_test.txt
    
    requirements

--- systemofrecord/__init__.py
+++ systemofrecord/__init__.py
@@ -7,13 +7,10 @@ app = Flask(__name__)
 
 app.config.from_object(os.environ.get('SETTINGS'))
 
-
 if not app.debug:
     app.logger.addHandler(logging.StreamHandler())
     app.logger.setLevel(logging.INFO)
 
-app.logger.info( "============")
-app.logger.info(app.config)
-app.logger.info("============")
+app.logger.info("\nConfiguration\n%s\n" % app.config)
 
 db = SQLAlchemy(app)
