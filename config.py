81c80519da1fc61d056905fedb716b6d1de2a14c
Adam Shimali <adam.shimali@gmail.com>
2014-07-15 16:00:42 +0100

    use TestConfig

--- config.py
+++ config.py
@@ -3,10 +3,13 @@ import os
 class Config(object):
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    REDIS_HOST = os.environ.get('REDIS_HOST', 'redis://user:@localhost:6379') #please let this pass for now :)
+    REDIS_HOST = os.environ.get('REDIS_HOST')
     REDIS_QUEUE_KEY = os.environ.get('REDIS_QUEUE_KEY')
 
 class DevelopmentConfig(Config):
     DEBUG = True
+
+class TestConfig(DevelopmentConfig):
+        REDIS_HOST = 'redis://user:@localhost:6379'
 
 
