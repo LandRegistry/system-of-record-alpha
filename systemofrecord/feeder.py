025e4263774b841bf79b702a86a8c220e3c90e0f
tsangsimon <simon.tsang@gmail.com>
2014-07-24 11:38:43 +0100

    pickle instead of ascii to/fro

--- systemofrecord/feeder.py
+++ systemofrecord/feeder.py
@@ -1,5 +1,6 @@
 from redis import Redis
 import urlparse
+import cPickle as pickle
 
 class FeederQueue(object):
 
@@ -8,6 +9,16 @@ class FeederQueue(object):
         url = urlparse.urlparse(app.config.get('REDIS_URL'))
         self.redis_queue = app.config.get('REDIS_QUEUE_KEY')
         self.redis = Redis(host=url.hostname, port=url.port, password=url.password)
+
+    def enqueue_pickle(self, number, json):
+        try:
+            json_pickle = pickle.dumps(json)
+            self.redis.rpush(self.redis_queue, json_pickle)
+            self.logger.info("Enqueued data for title %s: data: %s" % (number, json))
+            self.logger.info("Redis: %s" % self.redis)
+        except Exception, e:
+            self.logger.info(e)
+            self.logger.info("Couldn't pickle data for title %s: data: %s" % (number, json))
 
     def enqueue(self, number, json):
         try:
