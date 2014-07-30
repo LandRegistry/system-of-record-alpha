db16e81f803b5b6b299df950c22614a5d60bc817
mooreandrew <andrewmoore@Andrews-MacBook-Pro.local>
2014-07-30 14:49:14 +0100

    Hacking enviromnet to stop database errors killing the app

--- environment.sh
+++ environment.sh
@@ -3,5 +3,6 @@ export DATABASE_URL='postgresql://localhost/sysofrec'
 export REDIS_QUEUE_KEY='titles_queue'
 export REDIS_URL='redis://user:@localhost:6379'
 
+set +o errexit
 createuser -s sysofrec
 createdb -U sysofrec -O sysofrec sysofrec -T template0
