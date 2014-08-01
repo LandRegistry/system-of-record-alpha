901b4402c9c276f91902f110a1b726410b54e958
Adam Shimali <adam.shimali@gmail.com>
2014-08-01 10:32:44 +0100

    Move hard coded config variables to environment.sh and environment_test.sh

--- config.py
+++ config.py
@@ -2,18 +2,15 @@ import os
 
 class Config(object):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    REDIS_URL = os.environ.get('REDIS_URL')
-    REDIS_QUEUE_KEY = os.environ.get('REDIS_QUEUE_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    REDIS_URL = os.environ['REDIS_URL']
+    REDIS_QUEUE_KEY = os.environ['REDIS_QUEUE_KEY']
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    REDIS_QUEUE_KEY='titles_queue'
-    REDIS_URL='redis://user:@localhost:6379'
 
 class TestConfig(DevelopmentConfig):
     TESTING=True
-    SQLALCHEMY_DATABASE_URI = "sqlite://"
 
 class DockerConfig(Config):
     DEBUG = True
