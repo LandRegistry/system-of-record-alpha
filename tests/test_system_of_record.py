b375caf7ae50f14e2725ed77aa1daa29a95528cc
Mat Wall <mat.wall@digital.cabinet-office.gov.uk>
2014-09-10 12:20:31 +0100

    Deletes data created after running test. Runs against new postgress instance for tests

--- tests/test_system_of_record.py
+++ tests/test_system_of_record.py
@@ -3,9 +3,10 @@ import json
 import mock
 
 from systemofrecord import server
-
+from systemofrecord.models import Title
 from data import data_from_mint
 from data import title_id
+
 
 data_for_the_feeder = data_from_mint['title']
 
@@ -14,10 +15,12 @@ class SystemOfRecordTestCase(unittest.TestCase):
     def setUp(self):
         self.app = server.app.test_client()
 
+    def tearDown(self):
+        Title.query.delete()
+
     @mock.patch("systemofrecord.feeder.FeederQueue.enqueue")
-    def test_add_title_should_put_to_db_and_queue_data(self,  mock_enqueue):
+    def test_add_title_should_put_to_db_and_queue_data(self, mock_enqueue):
         self.app.put("/titles/%s" % title_id, data=json.dumps(data_from_mint), content_type="application/json")
-        #mock_put.assert_called_with(title_number, data_from_mint)
         mock_enqueue.assert_called_with(title_id, data_for_the_feeder)
 
     @mock.patch("redis.Redis.info")
@@ -27,9 +30,9 @@ class SystemOfRecordTestCase(unittest.TestCase):
 
     def test_get_known_title_gets_from_db(self):
         self.app.get("/titles/%s" % title_id)
-        #mock_db_get.assert_called_with(title_number)
+        # mock_db_get.assert_called_with(title_number)
 
     def test_get_returns_404_if_title_not_found(self):
         response = self.app.get("/titles/%s" % title_id)
-        #mock_db_get.assert_called_with(title_id)
+        # mock_db_get.assert_called_with(title_id)
         assert response.status_code == 404
