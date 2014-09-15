39346b74e6ab312afa66b118416fa768633b2b42
Mat Wall <mat.wall@digital.cabinet-office.gov.uk>
2014-09-15 12:26:08 +0100

    Modified to no-longer use title_number

--- systemofrecord/server.py
+++ systemofrecord/server.py
@@ -4,12 +4,12 @@ from systemofrecord import app
 from systemofrecord.controllers import load_title_controller, store_title_controller
 
 
-@app.route('/titles/<title_number>', methods=['GET'])
-def get_title(title_number):
-    return load_title_controller.load_object(title_number)
+@app.route('/titles/<object_id>', methods=['GET'])
+def get_object(object_id):
+    return load_title_controller.load_object(object_id)
 
 
-@app.route('/titles/<title_number>', methods=['PUT'])
-def store_title(title_number):
-    app.logger.info("Storing object [title_number: %s], [data: %s]" % (title_number, request.json))
-    return store_title_controller.store_title(title_number, request.json)
+@app.route('/titles/<object_id>', methods=['PUT'])
+def store_object(object_id):
+    app.logger.info("Storing object [object_id: %s], [data: %s]" % (object_id, request.json))
+    return store_title_controller.store_title(object_id, request.json)
