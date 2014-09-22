481e195ca35a60c3d0b129ffcaf701b69e01db81
Mat Wall <mat.wall@digital.cabinet-office.gov.uk>
2014-09-22 16:39:48 +0100

    Adds messages to queue in producer and validates

--- tests/test_ingest_queue_consumer.py
+++ tests/test_ingest_queue_consumer.py
@@ -2,10 +2,9 @@ from unittest import TestCase
 
 from systemofrecord.services import ingest_queue
 from system_of_record_message_fixtures import valid_message_without_tags
+from datatypes.exceptions import DataDoesNotMatchSchemaException
+from system_of_record_message_fixtures import invalid_message_without_object
 
 
 class IngestQueueConsumerTestCase(TestCase):
-    def test_can_consume_queue_of_messages(self):
-        ingest_queue.add_to_queue(valid_message_without_tags)
-        ingest_queue.add_to_queue(valid_message_without_tags)
-
+   pass
--- tests/test_ingest_queue_producer.py
+++ tests/test_ingest_queue_producer.py
@@ -1,7 +1,8 @@
 from tests.QueueUnittest import QueueUnittest
 
 from systemofrecord.services import ingest_queue_producer, ingest_queue
-from system_of_record_message_fixtures import valid_message_without_tags
+from system_of_record_message_fixtures import valid_message_without_tags, invalid_message_without_object
+from datatypes.exceptions import DataDoesNotMatchSchemaException
 
 
 class TestIngestQueueProducer(QueueUnittest):
@@ -10,3 +11,10 @@ class TestIngestQueueProducer(QueueUnittest):
         ingest_queue_producer.enqueue(valid_message_without_tags)
         ingest_queue_producer.enqueue(valid_message_without_tags)
         self.assertEqual(ingest_queue.queue_size(), 2)
+
+
+    def test_cannot_add_invalid_messages_to_queue(self):
+        self.assertEqual(ingest_queue.queue_size(), 0)
+        self.assertRaises(DataDoesNotMatchSchemaException, ingest_queue_producer.enqueue,
+                          invalid_message_without_object)
+        self.assertEqual(ingest_queue.queue_size(), 0)
