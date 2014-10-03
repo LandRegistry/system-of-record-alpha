7df5e9b0de18e0e62c0290f7b858391e01a73f6c
Juan Matthys Uys <opyate@gmail.com>
2014-10-03 10:39:43 +0100

    queue name for mint messages

--- environment.sh
+++ environment.sh
@@ -1,6 +1,6 @@
 export DATABASE_URL='postgresql://localhost/sysofrec'
 export REDIS_URL='redis://user:@localhost:6379'
-export INGEST_QUEUE_NAME='system-of-record-ingest'
+export INGEST_QUEUE_NAME='lr:queue:mint:'
 export INGEST_QUEUE_POLL_INTERVAL_IN_SECONDS=60
 export FEEDER_QUEUE_NAME='titles_queue'
 export CHAIN_QUEUE_NAME='chain_queue'
